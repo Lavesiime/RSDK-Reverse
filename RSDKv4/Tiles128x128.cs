a7fa36db0deb2bb46e1889a5a82634e9fe7b0fbb
Lave sIime <70488825+Lavesiime@users.noreply.github.com>
2023-12-02 15:31:24 -0600

    Add missing tile solidity type

--- RSDKv4/Tiles128x128.cs
+++ RSDKv4/Tiles128x128.cs
@@ -24,6 +24,7 @@
                     SolidTop,
                     SolidAllButTop,
                     SolidNone,
+                    SolidTopNoGrip,
                 }
 
                 /// <summary>
